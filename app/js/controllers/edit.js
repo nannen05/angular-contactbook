864efa298d496e0dcb956a975996c9dfe07e98be
nannen05 <nannen05@gmail.com>
2016-03-11 10:49:15 -0800

    refactored edit page to use ng-model-options

--- app/js/controllers/edit.js
+++ app/js/controllers/edit.js
@@ -9,28 +9,31 @@ myApp.controller('EditContactController', ['$scope', '$http', '$location', '$fir
 
 		$scope.updateContact = function() {
 
+			if($scope.hideAddress = true) {
+				$scope.editcontact.address = $scope.updateAddress.chosenPlace;
+			} 
+
 			$http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + 
-						$scope.updatecontact.chosenPlace).success(function(data){
+						$scope.editcontact.address).success(function(data){
 						
 			$scope.lat = data.results[0].geometry.location.lat;
 			$scope.lng = data.results[0].geometry.location.lng;
 
 				var updatedData = {
 						firstname: $scope.editcontact.firstname,
-						lastname: $scope.updatecontact.lastname,
-						email: $scope.updatecontact.email,
-						phone: $scope.updatecontact.phone,
-						address: $scope.updatecontact.chosenPlace,
+						lastname: $scope.editcontact.lastname,
+						email: $scope.editcontact.email,
+						phone: $scope.editcontact.phone,
+						address: $scope.editcontact.address,
 						latitude: $scope.lat,
 						longitude: $scope.lng,
 						date: Firebase.ServerValue.TIMESTAMP
 				};
 
-				console.log(updatedData);
-
-				// editcontactSingle.update(updatedData).then(function(){
-	 		// 		$location.path('/contact/' + $routeParams.eid);
-	 		// 	});
+				editcontactSingle.$save(updatedData).then(function(){
+					var contactRoute = $routeParams.eId;
+	 				$location.path('/contact/' + contactRoute);
+	 			});
 			});
 		};
 
